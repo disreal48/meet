e0a14258f926674c67faf349b16264b2f53abaed
disreal48 <disreal48@googlemail.com>
2024-05-18 15:35:20 +0200

    added Scatterplot

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import "./App.css";
+import CityEventsChart from "./components/CityEventsChart";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -53,6 +54,7 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
